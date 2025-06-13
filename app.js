65f111f279f7bb22572bc0ee6bc78c1792463190
Maria Eduarda Torres <maria.pereira96@fatec.sp.gov.br>
2025-06-13 19:29:37 +0000

    att

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const dotenv = require ('dotenv');
 const conectarBanco = require('./config/db');
 const contatoRoutes = require('./routes/contatoRoutes');
+const cors = require('cors');
 
-dotenv.config();
+require('dotenv').config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/contatos', contatoRoutes);
 
 conectarBanco();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+
+
+
--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-function conectarBanco() {
-    mongoose.connect(process.env.MONGO_URI)
-        .then(() => console.log('MongoDB conectado com sucesso'))
-        .catch((err) => {
-            console.error('Erro ao conectar ao MongoDB:', err);
-            process.exit(1); // Encerra a aplicação em caso de falha
-        });
-}
-
+const conectarBanco = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB conectado com sucesso!');
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB:', error);
+    process.exit(1); // Encerra o processo com erro
+  }
+};
 
 module.exports = conectarBanco;
