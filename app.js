8660f39261178e959bc05b0f6516684b9170c203
Maria Eduarda Torres <maria.pereira96@fatec.sp.gov.br>
2025-06-28 00:09:41 +0000

    att

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dotenv = require ('dotenv');
 const conectarBanco = require('./config/db');
 const contatoRoutes = require('./routes/contatoRoutes');
 const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const authMiddleware = require('./middleware/authMiddleware');
 
 require('dotenv').config();
 const app = express();
@@ -12,11 +14,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/contatos', contatoRoutes);
+app.use('/api/usuarios', userRoutes); // Rotas públicas
+app.use('/api/contatos', authMiddleware, contatoRoutes)
 
 conectarBanco();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
 
 
 
